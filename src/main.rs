12ff16e7657c5252bd99799d3700e494051b0176
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-06-14 23:35:38 -0500

    Initial import, basically works

---
+++ src/main.rs
@@ -0,0 +1,44 @@
+extern crate image;
+
+use std::env;
+use std::path::Path;
+use image::{
+    GenericImage,
+    DynamicImage
+};
+
+fn main() {
+   let args: Vec<String> = env::args().collect();
+
+   if args.len() != 3 {
+       println!("Usage: imgcompare img1 img2");
+       return;
+   }
+
+
+   let img1 = image::open(&Path::new(&args[1])).unwrap();
+   let img2 = image::open(&Path::new(&args[2])).unwrap();
+
+   let result = compare_imgs(img1, img2);
+
+   std::process::exit(match result {
+       true => 0,
+       false => -1
+   });
+}
+
+fn compare_imgs(img1: DynamicImage, img2: DynamicImage) -> bool {
+    let px1 = img1.pixels();
+    let px2 = img2.pixels();
+
+    return px1.zip(px2).all(|i| {
+        let (a, b) = i;
+        let (x1, y1, p1) = a;
+        let (x2, y2, p2) = b;
+
+        //println!("a: {:?}", a);
+        //println!("b: {:?}", b);
+        return x1 == x2 && y1 == y2 && p1 == p2;
+    });
+}
+
